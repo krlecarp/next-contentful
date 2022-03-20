1494c0c8073f9883f5c3e1b613c072132c95c290
krlecarp <kylec@surfacenet.com>
2022-03-19 22:48:42 -0700

    skeleton

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful"
 import Image from 'next/image'
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Skeleton from "../../components/Skeleton"
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -21,11 +22,12 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
 export async function getStaticProps({params}) {
+  if (!recipe) return <Skeleton />
 
   const { items } = await client.getEntries({
     content_type: 'recipe', 
